1a442efb0ca612be912ba605ca58dbdbe2c11d1a
Wagner Trezub <60133113+Wagner3UB@users.noreply.github.com>
2021-07-14 11:28:07 +0200

    Update reverseastring.js
    added some comments

--- reverseastring.js
+++ reverseastring.js
@@ -1,12 +1,13 @@
-// Reverse a string
+//Reverse a string
 
 let newStr = "";
 
 function reverseAString (string){
   for (let i = string.length - 1 ; i >= 0 ; i--){
     newStr = newStr + string[i];
-    console.log(newStr);
+    console.log(newStr); //hide this console.log if you don't wanna show the full process
   };
+  // console.log(newStr);  --> Use this consolo.log if you wanna show just the final result
 };
 
 let resultado = reverseAString("Wagner Trezub"); //Change my name to change the string
